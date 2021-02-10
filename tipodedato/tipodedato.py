c51a747d03ffee7a8798276d30efcbfe58230876
misael <misael.570@gmail.com>
2021-02-09 20:39:37 -0600

    newvalores

--- tipodedato/tipodedato.py
+++ tipodedato/tipodedato.py
@@ -1,6 +1,10 @@
 # TIPODE DATO
 nombredepersona = "Misael"
 edaddepersona = 27
+altura = 1.76
+Soltero = True
 # nombredepersona = 13
-print("Nombre :" +nombredepersona)
-print("Edad:" + str(edaddepersona))
+print("Nombre: " +nombredepersona)
+print("Edad: " + str(edaddepersona))
+print("Altura: " + str(altura))
+print("Soltero: " + str(Soltero))
