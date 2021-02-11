012007299d5a84985be01fdf84e0b52f279fb4c2
misael <misael.570@gmail.com>
2021-02-10 19:44:54 -0600

    op2

--- Operacionesmat/operacionesmat.py
+++ Operacionesmat/operacionesmat.py
@@ -9,8 +9,19 @@ resta = float(num1) - float(num2)
 multiplicacion = float(num1) * float(num2)
 division = float(num1) / float(num2)
 
+potencia1 = float(num1) ** float(num2)
+potencia2 = pow(float(num1), float(num2))
+raiz_cuad = pow(float(num1), float(1/2))
+raiz_cub = pow(float(num1), float(1/3))
+
+
+# Impresiones
 print(suma)
 print(type(suma))
 print(resta)
 print(multiplicacion)
 print(division)
+print(potencia1)
+print(potencia2)
+print(raiz_cuad)
+print(raiz_cub)
