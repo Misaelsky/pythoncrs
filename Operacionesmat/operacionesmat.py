949614ee0919d7840456f3eb676e12a9b57ab1a8
misael <misael.570@gmail.com>
2021-02-10 19:29:42 -0600

    operaciones

---
+++ Operacionesmat/operacionesmat.py
@@ -0,0 +1,16 @@
+# Realizar sum
+print("Escribe un numero:")
+num1 = input()
+num2 = input("Escribe otro numero:")
+
+# Operacion de suma
+suma = float(num1) + float(num2)
+resta = float(num1) - float(num2)
+multiplicacion = float(num1) * float(num2)
+division = float(num1) / float(num2)
+
+print(suma)
+print(type(suma))
+print(resta)
+print(multiplicacion)
+print(division)
