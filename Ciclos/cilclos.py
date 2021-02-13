ab66f524714ca0a16c603227ce8512175e3b2e03
misael <misael.570@gmail.com>
2021-02-12 21:14:34 -0600

    ciclos

---
+++ Ciclos/cilclos.py
@@ -0,0 +1,11 @@
+# Cliclo For
+arreglo = [0, 1, 2, 3, 4, 5]
+
+for i in arreglo: # range(0, 100, 2): # range(inicio, fin, incremento/decremento)
+    print(i)
+
+# Ciclo While
+i = 1
+while (i <= 10):
+    print(i)
+    i += 2 # i = i + 1
