40ea08c5c1c6da00d0c3a5ec2003c2c1ba544f58
misael <misael.570@gmail.com>
2021-02-15 21:10:29 -0600

    numero

--- Listanum/nums.py
+++ Listanum/nums.py
@@ -2,13 +2,22 @@
 
 arreglo = [0, 1, 2, 3, 4, 5]
 
-for i in range(-100, 100, 1):
+for i in range(0, -100, -1):
     
 
     residuo = i % 2
 
     if (i<0 and -100<i and residuo>0):
-        print(int(i.sort(Reverse = True)))
+        print(i)
+for i in range(0, 100, 2):
+    
+
+    residuo = i % 2
+
+    if (0<i and i<100 and residuo==0):
+        print(i)
+    else:
+        print("NO VALIDO")
         
 
 
