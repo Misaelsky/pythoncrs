6f360c3534fb0979ac001e4d7f7c5aeb8c295340
misael <misael.570@gmail.com>
2021-02-16 20:04:06 -0600

    numeros enteros

---
+++ Enteros/numin.py
@@ -0,0 +1,13 @@
+# Ciclos de entero
+num = int(input("Ingresa un número entero: "))
+
+while num>0:
+    try:
+        num = int(input("Ingresa un número entero: "))
+        
+
+
+        
+    except:
+        print("Dato inválido")
+
