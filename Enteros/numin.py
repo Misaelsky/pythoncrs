e576ae9fd8382883acda07ce53e463936de14d6d
misael <misael.570@gmail.com>
2021-02-16 21:08:07 -0600

    numin

--- Enteros/numin.py
+++ Enteros/numin.py
@@ -1,13 +1,30 @@
 # Ciclos de entero
 num = int(input("Ingresa un número entero: "))
+suma = 0
+suma= suma + num
+contador = 0
+
+
+
+if num>0:
+    contador += 1
 
 while num>0:
     try:
         num = int(input("Ingresa un número entero: "))
-        
-
-
-        
+        if num>0:
+            suma = suma + num
+            contador = contador + 1
+            promedio = suma / contador
+            
     except:
         print("Dato inválido")
+
+
+print(float(promedio))
+
+
+
+
+
 
