cd2bc92cf29a156d1f66a314617599283d94a997
misael <misael.570@gmail.com>
2021-02-17 20:00:19 -0600

    1

--- Fun/funt.py
+++ Fun/funt.py
@@ -5,3 +5,12 @@ def Print(a, b, c, d, e, f, g, h, i, j):
 arreglo = Print(10, 8, 3, 7, 14, 9, 13, 4, 75, 5)
 print(arreglo)
 
+def Print2(Lista):
+    for item in Lista:
+        print(item)
+
+Lista = [0,1,2]
+
+Print2(Lista)
+
+
