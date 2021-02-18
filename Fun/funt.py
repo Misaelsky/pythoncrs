b4d9ce23f2201a2c1a41204341537e53fb5719d9
misael <misael.570@gmail.com>
2021-02-17 20:53:14 -0600

    2

--- Fun/funt.py
+++ Fun/funt.py
@@ -7,9 +7,10 @@ print(arreglo)
 
 def Print2(Lista):
     for item in Lista:
-        print(item)
+        if item > 5:
+            print(item)
 
-Lista = [0,1,2]
+Lista = [0,1,2,3,4,5,6,7,8,9]
 
 Print2(Lista)
 
